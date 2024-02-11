3a8e67d3dbdf994c67cc098d0402a604870ea98d
arpan21020 <arpan21020@iiitd.ac.in>
2024-02-12 00:46:11 +0530

    generalized for any os

--- Assignment1/gRPC/Shopping Platform/Code/buyer.py
+++ Assignment1/gRPC/Shopping Platform/Code/buyer.py
@@ -3,6 +3,7 @@ from concurrent import futures
 import time
 import uuid
 import logging
+import sys
 
 # from shopping_pb2 import (
 #     SearchItemRequest,
@@ -88,11 +89,11 @@ def main(ip,port):
     logging.basicConfig()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     shopping_pb2_grpc.add_NotificationServicer_to_server(NotificationServicer(), server)
-    server.add_insecure_port("[::]:50054")
+    server.add_insecure_port(f"[::]:{port}")
     server.start()
     print("Buyer notification Server started")
-    buyer_address = "localhost:50054"
-    with grpc.insecure_channel("localhost:50052") as market_channel:
+    buyer_address = f"{ip}:{port}"
+    with grpc.insecure_channel(f"{ip}:{port}") as market_channel:
         market_stub = shopping_pb2_grpc.MarketStub(market_channel)
         # Replace with actual Market address
         # notify_channel = grpc.insecure_channel(
@@ -144,4 +145,6 @@ def main(ip,port):
 
 
 if __name__ == "__main__":
-    main()
+    ip=sys.argv[1]
+    port=sys.argv[2]
+    main(ip,port)
