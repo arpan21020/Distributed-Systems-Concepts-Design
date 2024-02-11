f03d9f806428cc067da032be3b267be48cd35b30
Raj Pratap Singh <rp_singh@Rajs-MacBook-Air.local>
2024-02-12 00:34:13 +0530

    Final gRPC

--- Assignment1/gRPC/Shopping Platform/Code/buyer.py
+++ Assignment1/gRPC/Shopping Platform/Code/buyer.py
@@ -1,23 +1,7 @@
 import grpc
 from concurrent import futures
-import time
 import uuid
 import logging
-
-# from shopping_pb2 import (
-#     SearchItemRequest,
-#     BuyItemRequest,
-#     AddToWishListRequest,
-#     RateItemRequest,
-#     NotifyClientRequest,
-#     NotifyClientResponse,
-# )
-# from shopping_pb2_grpc import (
-#     MarketStub,
-#     NotifyClientStub,
-#     NotifyClientServicer,
-#     add_NotifyClientServicer_to_server,
-# )
 
 import shopping_pb2
 import shopping_pb2_grpc
@@ -30,14 +14,6 @@ class NotificationServicer(shopping_pb2_grpc.NotificationServicer):
         return shopping_pb2.NotifyClientResponse(
             result=shopping_pb2.NotifyClientResponse.SUCCESS
         )
-
-
-# class NotifyClientServicerImpl(NotifyClientServicer):
-#     def NotifyClient(self, request, context):
-#         print("Buyer prints:")
-#         print(request.message)
-#         return NotifyClientResponse(result=NotifyClientResponse.SUCCESS)
-
 
 def search_item(stub, item_name, category):
     request = shopping_pb2.SearchRequest(name=item_name, category=category)
@@ -84,7 +60,7 @@ def rate_item(stub, item_id, buyer_address, rating):
     else:
         print("FAIL")
 
-def main(ip,port):
+def main():
     logging.basicConfig()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     shopping_pb2_grpc.add_NotificationServicer_to_server(NotificationServicer(), server)
@@ -94,19 +70,6 @@ def main(ip,port):
     buyer_address = "localhost:50054"
     with grpc.insecure_channel("localhost:50052") as market_channel:
         market_stub = shopping_pb2_grpc.MarketStub(market_channel)
-        # Replace with actual Market address
-        # notify_channel = grpc.insecure_channel(
-        #     "localhost:50052"
-        # )  # Replace with actual Notification address
-        # notify_stub = NotifyClientStub(notify_channel)
-
-        # Create a gRPC server for NotifyClient service
-        # notify_server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
-        # add_NotifyClientServicer_to_server(NotifyClientServicerImpl(), notify_server)
-        # notify_server.add_insecure_port(buyer_address)
-        # notify_server.start()
-
-        # Example usage of functionalities
         unique_id = str(uuid.uuid1())
         # menu driven
         while True:
